4dfc25e189bd55e4aa544746c27f1af69c8ca542
Oussama Moumni <moumnioussama96@gmail.com>
2022-11-28 08:46:58 +0100

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './assets/sass/style.scss';
 import "normalize.css/normalize.css";
-console.log("Hello");
+
+console.log("Hello from webpack");
+
+// (function () {
+//   const closeBanner = document.querySelectorAll(".c-banner__close");
+//   closeBanner.forEach((closeBanner) => {
+//     closeBanner.addEventListener("click", (e) => {
+//       const banner = e.target.parentNode;
+//         banner.classList.add("collapse");
+        
+//         banner.addEventListener('transitionend', function (e) {
+//             if (e.target === this) {
+//                 this.remove()
+//             }
+//         })
+//     });
+//   });
+// })();
--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,12 @@ module.exports = {
   //   mode: "none",
   entry: {
     app: "./src/index.js",
+    "assets/js/banner": "./src/assets/js/banner.js",
   },
   output: {
-    publicPath:'/',
+    publicPath: "/",
     path: path.join(__dirname, "/app"),
-    filename: "app.js",
+    filename: "[name].js",
   },
   devServer: {
     devMiddleware: {
@@ -34,9 +35,24 @@ module.exports = {
         },
       },
       {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
         test: /\.(sass|css|scss)$/,
 
-        use: [MiniCssExtractPlugin.loader, "css-loader","postcss-loader", "sass-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(svg|eot|woff|woff2|ttf)$/,
@@ -62,6 +78,27 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/index.html",
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "components/button.html",
+      template: "./src/components/button.html",
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "components/textfield.html",
+      template: "./src/components/textfield.html",
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "components/card.html",
+      template: "./src/components/card.html",
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "components/banner.html",
+      template: "./src/components/banner.html",
+      chunks: ["app", "assets / js / banner"],
     }),
     new CssMinimizerPlugin({}),
 
