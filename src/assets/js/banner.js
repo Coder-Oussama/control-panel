b19e3841aebbefb8ab6f5d0d8c98f28de2afcea0
Oussama Moumni <moumnioussama96@gmail.com>
2022-12-01 00:30:39 +0100

    finish components

--- src/assets/js/banner.js
+++ src/assets/js/banner.js
@@ -1,6 +1,6 @@
 console.log('hello from banner.js');
 (function () {
-  const closeBanner = document.querySelectorAll(".c-banner__close");
+  const closeBanner = document.querySelectorAll(".js-banner__close");
   closeBanner.forEach((closeBanner) => {
     closeBanner.addEventListener("click", (event) => {
       const banner = event.target.parentNode;
